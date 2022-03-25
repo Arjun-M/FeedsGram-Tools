6a6ee059addf68827f297a83b3e9905ed040af5c
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-03-25 21:16:59 +0530

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -4,15 +4,21 @@ const axios = require('axios')
 
 connectDB()
 
+const API = async (id , token , payload) => {
+  try{
+    let resp = await axios.post("https://api.telegram.org/bot"+token+"/sendPhoto" , payload)
+    console.log ("➢ "+resp.data.ok+" from "+id)
+  }catch(err){
+    console.log (`Error ${err.data} from ${id}`)
+  }
+}
+ 
 
 DB.find({}).then(dd=>{
-  for ( var x in dd ){
-    axios.post("https://api.telegram.org/bot"+dd[x].token+"/sendMessage?").then(d=>{    
-      console.log ( d.data.toString()+""+dd[x]._id)
-    }).catch ( err=>{
-      console.log ("Error :  "+ dd[x]._id)      
-    })
+  for ( var x in dd ){  
+    API("01010", , {chat_id:2087092793, photo:"https://t.me/itz_Arjun_M/4" , caption:"*😁 Need Help With FeedsGram ?*\n\nWatch this quick [tutorial](https://youtu.be/rstQ6fleK5g) for help .\n\nJoin @FeedsGram to stay in touch with us ." , parse_mode:"Markdown"}) 
   }
 })
 
 return
+
